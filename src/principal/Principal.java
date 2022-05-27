60e3166fe9948c81d8cb10cd5ed0d128f6318308
RYZEN <RYZEN@DESKTOP-PT59ORD>
2022-05-27 11:46:53 +0200

    Commit 1 master

---
+++ src/principal/Principal.java
@@ -0,0 +1,17 @@
+package principal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public double sumar(double a, double b) {
+        return 1.0;
+    }
+    
+    public double multiplicar(double a, double b) {
+        return 1.0;
+    }
+    
+}
